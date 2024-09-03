04d921ee674ed3bc45a71ce8853c9f2c6b8c7e43
Ahmed Mohamed Shawky <Ahmed.M.Shawky98@gmail.com>
2024-09-03 21:52:57 +0300

    Update Age Calculator main.c

--- Day 2 Tasks/Age Calculator/main.c
+++ Day 2 Tasks/Age Calculator/main.c
@@ -4,57 +4,50 @@
 int main()
 {
     printf("------------------------Age Calculator App------------------------\n");
-    int DD,MM,YY;
-    int C_DD=22,C_MM=10,C_YY=2023;
-    int R_DD,R_MM,R_YY,MM_Days;
+    int DD, MM, YY;
+    int C_DD = 3, C_MM = 9, C_YY = 2024;
+    int R_DD, R_MM, R_YY, MM_Days;
+
     do
     {
-        printf("Enter Your B-Day with this format \"DD-MM-YY\": ");
-        scanf("%i-%i-%i",&DD,&MM,&YY);
-        _flushall();
+        printf("\nEnter Your Birth Date in the format \"DD-MM-YYYY\": ");
+        scanf("%d-%d-%d", &DD, &MM, &YY);
+        fflush(stdin);  // Flush input buffer
+
         switch(MM)
         {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 MM_Days = 31;
                 break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
+            case 4: case 6: case 9: case 11:
                 MM_Days = 30;
                 break;
             case 2:
-                MM_Days = (YY%4!=0) ? 28 : 29;
+                MM_Days = (YY % 4 == 0 && (YY % 100 != 0 || YY % 400 == 0)) ? 29 : 28;
                 break;
+            default:
+                MM_Days = 0;  // Invalid month
         }
-    }while(DD<0||DD>MM_Days||MM<1||MM>12||YY<1980||YY>2023);
+    } while(DD < 1 || DD > MM_Days || MM < 1 || MM > 12 || YY < 1980 || YY > C_YY);
 
-    R_YY = C_YY-YY;
-    if(C_MM>DD)
-        R_MM = C_MM-MM;
-    else
-    {
-        R_YY--;
-        R_MM = (C_MM+12)-MM;
-    }
-    if(C_DD>DD)
-        R_DD = C_DD-DD;
-    else
+    // Calculate age
+    R_YY = C_YY - YY;
+    R_MM = C_MM - MM;
+    R_DD = C_DD - DD;
+
+    if (R_DD < 0)
     {
         R_MM--;
-        R_DD = (C_DD+MM_Days)-DD;
-        if(MM=2)
-        {
-            if(YY%4!=0)R_DD+=2;
-            else R_DD++;
-        }
+        R_DD += MM_Days;
     }
-    printf("day: %i Month: %i Year: %i",R_DD,R_MM,R_YY);
+
+    if (R_MM < 0)
+    {
+        R_YY--;
+        R_MM += 12;
+    }
+
+    printf("\nYour age is: %d years, %d months, and %d days\n", R_YY, R_MM, R_DD);
+
     return 0;
 }
